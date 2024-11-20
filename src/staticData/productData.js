1bd041316770ac3da859ca9bd6389d96cee4b47d
nayfort <mitrofanovvladislav2004@gmail.com>
2024-11-20 17:12:55 +0200

    edit products data

--- src/staticData/productData.js
+++ src/staticData/productData.js
@@ -9,14 +9,14 @@ import { bottleFront } from "../assets/imgs/png/bottle/index.jsx";
 import {Bag, Bottle, Cap, Cups, Hat, Hoodie, Stickers, Tshirt} from "../assets/imgs/png/index.js";
 
 export const productsData = [
-    { id: 1, name: 'Футболка', price: 50, img: Tshirt, type: 'apparel' },
-    { id: 2, name: 'Худі', price: 60, img: Hoodie, type: 'apparel' },
-    { id: 3, name: 'Шапка', price: 25, img: Hat, type: 'accessory' },
-    { id: 4, name: 'Кепка', price: 20, img: Cap, type: 'accessory' },
-    { id: 5, name: 'Шопер', price: 15, img: Bag, type: 'accessory' },
-    { id: 6, name: 'Стакан', price: 10, img: Cups, type: 'accessory' },
-    { id: 7, name: 'Стікер', price: 5, img: Stickers, type: 'accessory' },
-    { id: 8, name: 'Пляшка', price: 12, img: Bottle, type: 'accessory' },
+    { id: 1, name: 'Футболка', price: 50, img: Tshirt, type: 'apparel', category: 'Одяг' },
+    { id: 2, name: 'Худі', price: 60, img: Hoodie, type: 'apparel', category: 'Одяг' },
+    { id: 3, name: 'Шапка', price: 25, img: Hat, type: 'accessory', category: 'Одяг' },
+    { id: 4, name: 'Кепка', price: 20, img: Cap, type: 'accessory', category: 'Одяг' },
+    { id: 5, name: 'Шопер', price: 15, img: Bag, type: 'accessory', category: 'Аксесуари' },
+    { id: 6, name: 'Стакан', price: 10, img: Cups, type: 'accessory', category: 'Аксесуари' },
+    { id: 7, name: 'Стікер', price: 5, img: Stickers, type: 'accessory', category: 'Аксесуари' },
+    { id: 8, name: 'Пляшка', price: 12, img: Bottle, type: 'accessory', category: 'Аксесуари' },
 ];
 
 export const productImages = {
